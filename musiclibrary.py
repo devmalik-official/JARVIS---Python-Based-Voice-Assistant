630d5c0111234cddfa44b921853e2c5214e82022
devmalik-official <devmalik.mail@gmail.com>
2025-09-21 11:32:41 +0530

    Update musiclibrary.py

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth" : "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "introspective" : "https://www.youtube.com/watch?v=TOsvDAw47ss",
+    "beliver" : "https://www.youtube.com/watch?v=W0DM5lcj6mw&list=RDU47Tr9BB_wE&index=4",
+    "mortals" : "https://www.youtube.com/watch?v=1sIZe6BU8j4&list=RDU47Tr9BB_wE&index=5",  
+}
